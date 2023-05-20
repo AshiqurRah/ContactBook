3729787c6b16a0746fd5303e850359223ba6776e
Ashiqur Rahman <e0689695@u.nus.edu>
2023-05-20 18:17:59 +0800

    Update initial child route

--- src/main.jsx
+++ src/main.jsx
@@ -11,6 +11,7 @@ import ErrorPage from "./error-page";
 import Contact, {loader as contactLoader,} from "./routes/contact";
 import EditContact, {action as editAction} from "./routes/edit";
 import { action as destroyAction } from "./routes/destroy";
+import Index from "./routes/index";
 
 
 const router = createBrowserRouter([
@@ -24,6 +25,7 @@ const router = createBrowserRouter([
     loader: rootLoader,
     // children route, nested routes
     children: [
+      { index: true, element: <Index /> },
       {
         path: "contacts/:contactId",
         element: <Contact />,
@@ -38,6 +40,7 @@ const router = createBrowserRouter([
       {
         path: "contacts/:contactId/destroy",
         action: destroyAction,
+        errorElement: <div>Oops! There was an error.</div>,
       },
     ],
   },
