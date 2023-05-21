b3053daec7e4910810f5035956c7f13b877125d9
Ashiqur Rahman <e0689695@u.nus.edu>
2023-05-21 13:16:52 +0800

    Improved error page

--- src/main.jsx
+++ src/main.jsx
@@ -25,23 +25,18 @@ const router = createBrowserRouter([
     loader: rootLoader,
     // children route, nested routes
     children: [
-      { index: true, element: <Index /> },
       {
-        path: "contacts/:contactId",
-        element: <Contact />,
-        loader: contactLoader,
-        action: contactAction,
-      },
-      {
-        path: "contacts/:contactId/edit",
-        element: <EditContact />,
-        loader: contactLoader,
-        action: editAction,
-      },
-      {
-        path: "contacts/:contactId/destroy",
-        action: destroyAction,
-        errorElement: <div>Oops! There was an error.</div>,
+        errorElement: <ErrorPage />,
+        children: [
+          { index: true, element: <Index /> },
+          {
+            path: "contacts/:contactId",
+            element: <Contact />,
+            loader: contactLoader,
+            action: contactAction,
+          },
+          /* the rest of the routes */
+        ],
       },
     ],
   },
--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -34,7 +34,7 @@ export default function Root() {
     return (
         <>
             <div id="sidebar">
-                <h1>React Router Contacts</h1>
+                <h1>ContactBook by React Router</h1>
                 <div>
                     <Form id="search-form" role="search">
                         <input
